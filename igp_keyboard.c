4ace29f0c720405e1d64b6fa32e38ba4dfb73495
Sky_miner <458482582@qq.com>
2019-05-22 20:22:51 +0800

    delete some ...

--- igp_keyboard.c
+++ igp_keyboard.c
@@ -15,126 +15,28 @@
 #include <ocidl.h>
 #include <winuser.h>
 
-#define deltax 0.03
-#define deltay 0.03
+void KeyboardEventProcess(int key,int event);/*锟斤拷锟斤拷锟斤拷息锟截碉拷锟斤拷锟斤拷*/
 
-static double ccx = 1.0, ccy = 1.0;/*圆心坐标*/
-static double radius = 1.0; /*圆半径*/
-
-void DrawCenteredCircle(double x, double y, double r);/*画中心圆*/
-void KeyboardEventProcess(int key,int event);/*键盘消息回调函数*/
-
-void Main() /*仅初始化执行一次*/
-{
-    InitGraphics();        	
-	
-	registerKeyboardEvent(KeyboardEventProcess);/*注册键盘消息回调函数*/
-
-	SetPenColor("Red"); 
-    SetPenSize(1);
+void Main(){
     
-    ccx = GetWindowWidth()/2;
-    ccy = GetWindowHeight()/2;
-    DrawCenteredCircle(ccx, ccy, radius);
 }
 
-void DrawCenteredCircle(double x, double y, double r)
-{
-    MovePen(x + r, y);
-    DrawArc(r, 0.0, 360.0);
-}
-
-void KeyboardEventProcess(int key,int event)/*每当产生键盘消息，都要执行*/
-{
- 	 double oldradius;
- 	 
-     switch (event) {
+void KeyboardEventProcess(int key,int event){
+ 	
+    switch (event) {
 	 	case KEY_DOWN:
 			 switch (key) {
-			     case VK_UP:/*UP*/
-			         SetEraseMode(TRUE);/*擦除前一个*/
-                     DrawCenteredCircle(ccx, ccy, radius);
-					 ccy += deltay;
-					 SetEraseMode(FALSE);/*画新的*/
-                     DrawCenteredCircle(ccx, ccy, radius);
+			     case VK_UP:
+
                      break;
 			     case VK_DOWN:
-			         SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-					 ccy -= deltay;
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
+
                      break;
 			     case VK_LEFT:
-			         SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-					 ccx -= deltax;
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
+
                      break;
 			     case VK_RIGHT:
-			         SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-					 ccx += deltax;
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     break;
-			     case VK_F1:
-  					 SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-		 	         SetPenSize(GetPenSize()+1);
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-					 break;
-			     case VK_F2:
-  					 SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-		 	         SetPenSize(GetPenSize()-1);
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     break;
-			     case VK_F3:
-			     case VK_PRIOR:
-	 	     		 SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     radius += 0.1;
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     break;
-			     case VK_F4:
-			     case VK_NEXT:
-		 	         SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     radius -= 0.1;
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     break;
-			     case VK_F5:
-                     break;
-			     case VK_F6:
-                     break;
-			     case VK_F9:
-			         InitConsole();
-			         oldradius = radius;
-			         printf("Input radius: ");
-			         radius = GetReal();
-			         FreeConsole();
-  					 SetEraseMode(TRUE);
-                     DrawCenteredCircle(ccx, ccy, oldradius);
-					 SetEraseMode(FALSE);
-                     DrawCenteredCircle(ccx, ccy, radius);
-                     break;
-			     case VK_ESCAPE:/*设置光标闪烁标志*/
-/*
-                     isBlink = !isBlink;
-                     if (isBlink ) {
-						startTimer(TIMER_BLINK500, mseconds500);
-						startTimer(TIMER_BLINK1000, mseconds1000);
-                     } else {
-						cancelTimer(TIMER_BLINK500);
-    					cancelTimer(TIMER_BLINK1000);
-					 }
-*/
+
                      break;
 			 }
 			 break;
